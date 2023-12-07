f36d960827461fe076e5cd54fcc4fe957f9098f9
designingworld <care.designingworld@gmail.com>
2023-12-07 20:20:55 +0600

    Review Form Added

--- assets/js/spromoter-front.js
+++ assets/js/spromoter-front.js
@@ -1,1 +1,54 @@
-// TODO:: Core here
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Showing Reviews
+  const reviewContainer = document.querySelectorAll('.spromoter-single-review');
+
+  reviewContainer.forEach(function(element) {
+      var author = element.getAttribute('data-spromoter-comment-author');
+      var content = element.getAttribute('data-spromoter-comment-content');
+
+      var reviewContent = 'Name: ' + author + '<br>' +
+                        'Description: ' + content + '<br>'
+      element.innerHTML = reviewContent;
+  });
+
+  // Review Form
+  const reviewForm = document.getElementById('spromoter-reviews-form');
+
+    var reviewFormHTML = "" + 
+    "<form class='spromoter-review-form' action='post'>" +
+      "<input type='text' id='' name='' class='spromoter-form-input' placeholder='Name'>" +
+      "<input type='email' id='' name='' class='spromoter-form-input' placeholder='Email'>" +
+      "<fieldset class='spromoter-rating' id=''>" +
+        "<input type='radio' id='star5' name='rating' value='5'>" +
+        "<label class='full' for='star5' title='Awesome - 5 stars'></label>" +
+        "<input type='radio' id='star4half' name='rating' value='4 and a half'>" +
+        "<label class='half' for='star4half' title='Pretty good - 4.5 stars'></label>" +
+        "<input type='radio' id='star4' name='rating' value='4'>" +
+        "<label class='full' for='star4' title='Pretty good - 4 stars'></label>" +
+        "<input type='radio' id='star3half' name='rating' value='3 and a half'>" +
+        "<label class='half' for='star3half' title='Meh - 3.5 stars'></label>" +
+        "<input type='radio' id='star3' name='rating' value='3'>" +
+        "<label class='full' for='star3' title='Meh - 3 stars'></label>" +
+        "<input type='radio' id='star2half' name='rating' value='2 and a half'>" +
+        "<label class='half' for='star2half' title='Kinda bad - 2.5 stars'></label>" +
+        "<input type='radio' id='star2' name='rating' value='2'>" +
+        "<label class='half' for='star2' title='Kinda bad - 2 stars'></label>" +
+        "<input type='radio' id='star1half' name='rating' value='1 and a half'>" +
+        "<label class='half' for='star1half' title='Meh - 1.5 stars'></label>" +
+        "<input type='radio' id='star1' name='rating' value='1'>" +
+        "<label class='half' for='star1' title='Sucks big time - 1 star'></label>" +
+        "<input type='radio' id='starhalf' name='rating' value='half'>" +
+        "<label class='half' for='starhalf' title='Sucks big time - 0.5 stars'></label>" +
+      "</fieldset>" +
+      "<textarea name='' id='' class='spromoter-form-input' placeholder='Comment'></textarea>" +
+      "<input class='spromoter-form-input' type='file' id='formFile'>" +
+      "<div class='spromoter-form-check'>" +
+        "<input class='spromoter-form-check-input' type='checkbox' value='' id='flexCheckDefault'>" +
+        "<label class='spromoter-form-check-label' for='flexCheckDefault'>Default checkbox</label>" +
+      "</div>" +
+      "<button type='submit' class='spromoter-form-button'>Submit</button>" +
+    "</form>";
+
+    reviewForm.innerHTML = reviewFormHTML;
+});
--- spromoter.php
+++ spromoter.php
@@ -78,23 +78,50 @@ function spromoter_show_main_widget_in_tab($tabs) {
 	}
 }
 
+
 function spromoter_show_reviews_widget() {
 	global $product;
 
 	if ( $product->get_reviews_allowed() == true ) {
 		$product_data = spromoter_get_product_data($product);
 
-		echo "<div class='spromoter spromoter-main-widget'
-		data-spromoter-product-id='".$product_data['id']."'
-		data-spromoter-name='".$product_data['title']."'
-		data-spromoter-url='".$product_data['url']."'
-		data-spromoter-image-url='".$product_data['image-url']."'
-		data-spromoter-price='".$product->get_price()."'
-		data-spromoter-currency='".get_woocommerce_currency()."'
-		data-spromoter-description='".$product_data['description']."'
-	></div>";
+		echo "<div class='spromoter-container'>
+			<div class='spromoter-reviews'>
+				<div class='spromoter-single-review'
+					data-spromoter-comment-author='".$product_data['id']."'
+					data-spromoter-comment-avater='".$product_data['title']."'
+					data-spromoter-comment-content='".$product_data['title']."'
+					data-spromoter-comment-ratings='".$product_data['url']."'
+					data-spromoter-comment-date='".$product_data['image-url']."'
+				></div>
+			</div>
+			<div id='spromoter-reviews-form'></div>
+	</div>";
 	}
 }
+
+// function spromoter_show_reviews_widget() {
+// 	global $product;
+
+// 	if ( $product->get_reviews_allowed() == true ) {
+// 		$product_data = spromoter_get_product_data($product);
+
+// 		echo "<div class='spromoter-container'>
+// 			<div class='spromoter-reviews'>
+// 				<div class='spromoter-single-review'
+// 					data-spromoter-product-id='".$product_data['id']."'
+// 					data-spromoter-name='".$product_data['title']."'
+// 					data-spromoter-url='".$product_data['url']."'
+// 					data-spromoter-image-url='".$product_data['image-url']."'
+// 					data-spromoter-price='".$product->get_price()."'
+// 					data-spromoter-currency='".get_woocommerce_currency()."'
+// 					data-spromoter-description='".$product_data['description']."'
+// 				></div>
+// 			</div>
+// 			<div id='spromoter-reviews-form'></div>
+// 	</div>";
+// 	}
+// }
 
 function spromoter_get_product_data($product) {
 	$settings = get_option('spromoter_settings',spromoter_get_default_settings());
@@ -231,7 +258,5 @@ function spromoter_get_small_logo_url()
 function declare_hops_support() {
 	if ( class_exists( FeaturesUtil::class ) ) {
 		FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
-
-
 	}
 }
