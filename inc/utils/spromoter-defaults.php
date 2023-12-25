cb83205b3f081d767be6c98251f95bd8a80f0a34
designingworld <care.designingworld@gmail.com>
2023-12-25 21:45:30 +0600

    fix css

--- inc/utils/spromoter-defaults.php
+++ inc/utils/spromoter-defaults.php
@@ -5,7 +5,7 @@ function spromoter_get_default_settings(){
 		'app_id' => '',
 		'api_key' => '',
 		'order_status' => 'completed',
-		'review_show_in' => 'footer',
+		'review_show_in' => 'tab',
 		'disable_native_review_system' => true,
 		'show_bottom_line_widget' => true,
 	);
