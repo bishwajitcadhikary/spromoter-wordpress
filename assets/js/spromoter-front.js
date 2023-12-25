342ee8c295bfbb26617aa766e86871761146d42c
designingworld <care.designingworld@gmail.com>
2023-12-25 20:15:40 +0600

    fixed js

--- assets/js/spromoter-front.js
+++ assets/js/spromoter-front.js
@@ -1,8 +1,7 @@
-(function() {
+document.addEventListener('DOMContentLoaded', function () {
     let widget = document.createElement('script');
     widget.type = 'text/javascript';
     widget.async = true;
     widget.src = `//staticwp.spromoter.test/${spromoterSettings.app_id}/widget.js`;
-    let script = document.getElementsByTagName('script')[0];
-    script.parentNode.insertBefore(widget, script);
-})();
+    document.body.appendChild(widget);
+});
